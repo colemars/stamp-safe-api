64d2d398d0ddbe64052a69eb329bed5bd50845c4
Cole Marsteller <cgm32547@gmail.com>
2019-07-18 00:35:21 -0700

    potentially working debugging env somehow

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": ["error"]
+  },
+  plugins: [
+    "prettier",
+  ]
+};
