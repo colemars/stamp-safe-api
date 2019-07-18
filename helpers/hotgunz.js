53f9dacbed70ca523943aa8c7eab721b8cefc7c8
Cole Marsteller <cgm32547@gmail.com>
2019-07-18 00:52:57 -0700

    lint and prettier hotgunz function

--- helpers/hotgunz.js
+++ helpers/hotgunz.js
@@ -1,30 +1,20 @@
-const fetch = require('node-fetch');
-const { URLSearchParams } = require('url');
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
+import fetch from "node-fetch";
+import { URLSearchParams } from "url";
 
-const getStolenRecord = (serialNumber) => new Promise((resolve, reject) => {
+const getStolenRecord = serialNumber =>
+  new Promise((resolve, reject) => {
+    const params = new URLSearchParams();
+    params.append("q", serialNumber);
+    const options = {
+      method: "POST",
+      body: params
+    };
 
-  const params = new URLSearchParams();
-  params.append('q', serialNumber);
-  
-  const options = {
-    method: 'POST',
-    body: params
-  };
-
-  fetch("https://www.hotgunz.com/search.php", options)
-            .then(res => res.text())
-            // .then(function(body){
-            //   const dom = new JSDOM(body);
-            //   const document = dom.window.document;
-            //   const result = document.querySelector('#flash');
-            //   console.log(result)
-            // })
-            .then(body => resolve(body))
-            .catch(error => reject(error));
-});
-
-module.exports = {
-  getStolenRecord,
+    fetch("https://www.hotgunz.com/search.php", options)
+      .then(res => res.text())
+      .then(body => resolve(body))
+      .catch(error => reject(error));
+  });
+export default {
+  getStolenRecord
 };
