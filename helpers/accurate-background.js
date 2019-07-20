4f68d5b5fcb113adee126a401f2d0ad6c75ea3d8
Cole Marsteller <cgm32547@gmail.com>
2019-07-19 23:18:54 -0700

    update helper functions to use passed params

--- helpers/accurate-background.js
+++ helpers/accurate-background.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
-const createCustomer = () =>
+const createCandidate = candidateInformation =>
   new Promise((resolve, reject) => {
     const dataString = JSON.stringify({
-      firstName: "Albert",
-      lastName: "Einstein",
-      phone: "206-555-1212",
-      dateOfBirth: "1972-05-26",
-      ssn: "531-90-1234",
-      email: "bert@physics.org"
+      firstName: candidateInformation.firstName,
+      lastName: candidateInformation.lastName,
+      phone: candidateInformation.phone,
+      dateOfBirth: candidateInformation.dateOfBirth,
+      ssn: candidateInformation.ssn,
+      email: candidateInformation.email
     });
     const uri = "https://api.accuratebackground.com/v3/candidate/";
     const clientId = "b97083ec-d9c1-4d5f-b88c-06c43b4eed0f";
@@ -27,38 +27,36 @@ const createCustomer = () =>
     fetch(uri, options)
       .then(res => res.json()) // expecting a json response
       .then(json => console.log(JSON.stringify(json)))
-      .then(body => resolve(body))
+      .then(json => resolve(json))
       .catch(error => reject(error));
   });
 
-const placeOrder = () => {
-  const uri = "https://api.accuratebackground.com/v3/order/";
-
-  const clientId = "b97083ec-d9c1-4d5f-b88c-06c43b4eed0f";
-  const clientSecret = "7f90b0d1-33ca-4bba-9418-e1e4bcd3ab13";
-
-  const headers = new fetch.Headers();
-  headers.append("Content-Type", "application/x-www-form-urlencoded");
-  headers.append(
-    "Authorization",
-    `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString("base64")}`
-  );
-
-  const param = new URLSearchParams();
-  param.append("candidateId", "5d3283587303d8316436bd72");
-  param.append("packageType", "PKG_BASIC");
-  param.append("workflow", "EXPRESS");
-  param.append("jobLocation.country", "US");
-  param.append("jobLocation.region", "CA");
-  param.append("jobLocation.city", "Hollywood");
-
-  const options = {
-    method: "POST",
-    headers,
-    body: param
-  };
-
-  fetch(uri, options)
-    .then(res => res.json())
-    .then(json => console.log(JSON.stringify(json)));
-};
+const placeOrder = candidateInformation =>
+  new Promise((resolve, reject) => {
+    const uri = "https://api.accuratebackground.com/v3/order/";
+    const clientId = "b97083ec-d9c1-4d5f-b88c-06c43b4eed0f";
+    const clientSecret = "7f90b0d1-33ca-4bba-9418-e1e4bcd3ab13";
+    const headers = new fetch.Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    headers.append(
+      "Authorization",
+      `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString("base64")}`
+    );
+    const param = new URLSearchParams();
+    param.append("candidateId", candidateInformation.Id);
+    param.append("packageType", "PKG_BASIC");
+    param.append("workflow", "EXPRESS");
+    param.append("jobLocation.country", candidateInformation.country);
+    param.append("jobLocation.region", candidateInformation.region);
+    param.append("jobLocation.city", candidateInformation.city);
+    const options = {
+      method: "POST",
+      headers,
+      body: param
+    };
+    fetch(uri, options)
+      .then(res => res.json())
+      .then(json => console.log(JSON.stringify(json)))
+      .then(json => resolve(json))
+      .catch(error => reject(error));
+  });
