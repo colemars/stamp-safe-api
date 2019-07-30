c150b4daeebc7c082c01d741096ca344e7fdc45b
Cole Marsteller <cgm32547@gmail.com>
2019-07-30 15:19:08 -0700

    refactor to handle both buyer and seller reports

--- create-report.js
+++ create-report.js
@@ -4,53 +4,70 @@ import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
   const data = JSON.parse(event.body);
+  const newLinkKey = uuid.v4();
+  const newAccessToken = uuid.v4();
+  const sellerReport = "100";
+  const buyerReport = "101";
+  const reportType = data.typeId === sellerReport ? sellerReport : buyerReport;
+  const newAccessKey = reportType === sellerReport ? data.accessKey : uuid.v4();
+  const returnValues =
+    reportType === sellerReport
+      ? { accessKey: newAccessKey, linkKey: newLinkKey }
+      : { newAccessToken };
+  const conditionalAttributes = {};
 
-  const stagesParams = {
-    TableName: "stages",
-    Key: {
-      stageId: data.stageId
+  if (reportType === sellerReport) conditionalAttributes.linkedReports = [];
+  if (reportType === buyerReport) {
+    conditionalAttributes.stolenPropertyCheckStatus = false;
+    conditionalAttributes.priceAlertStatus = false;
+    conditionalAttributes.accessToken = newAccessToken;
+  }
+
+  const params = {
+    TableName: "stampsafe-reports",
+    Item: {
+      typeId: reportType,
+      accessKey: newAccessKey,
+      linkKey: newLinkKey,
+      serialNumber: data.serialNumber,
+      make: data.make,
+      model: data.model,
+      yearManufactored: data.yearManufactored,
+      conditionOfItem: data.conditionOfItem,
+      imageKeys: data.imageKeys,
+      previousOwners: data.previousOwners,
+      price: data.price,
+      backgroundCheckStatus: false,
+      linkedReport: data.linkedReport,
+      accessToken: conditionalAttributes.accessToken,
+      stolenPropertyCheckStatus:
+        conditionalAttributes.stolenPropertyCheckStatus,
+      priceAlertStatus: conditionalAttributes.priceAlertStatus,
+      linkedReports: conditionalAttributes.linkedReports,
+      createdAt: Date.now()
     }
   };
 
   try {
-    const result = await dynamoDbLib.call("get", stagesParams);
-    if (result.Item) {
-      const reportsParams = {
-        TableName: "reports",
-        Item: {
-          reportId: uuid.v4(),
-          stageId: data.stageId,
-          serialNumber: result.Item.serialNumber,
-          make: result.Item.make,
-          model: result.Item.model,
-          yearManufactored: result.Item.yearManufactored,
-          conditionOfItem: result.Item.conditionOfItem,
-          image: result.Item.image,
-          previousOwners: result.Item.previousOwners,
-          price: result.Item.price,
-          backgroundCheckStatus: null,
-          stolenPropertyCheckStatus: null,
-          priceAlertStatus: null,
-          createdAt: Date.now()
-        }
-      };
-      const updateStageParams = {
-        TableName: "stages",
+    await dynamoDbLib.call("put", params);
+    if (reportType === buyerReport) {
+      const updateSellerReportParams = {
+        TableName: "stampsafe-reports",
         Key: {
-          stageId: data.stageId
+          typeId: sellerReport,
+          accessKey: data.accessKey
         },
         UpdateExpression:
-          "SET linkedReportIds = list_append(linkedReportIds, :linkedReportIds)",
+          "SET linkedReports = list_append(linkedReports, :linkedReports)",
         ExpressionAttributeValues: {
-          ":linkedReportIds": [reportsParams.Item.reportId.toString()]
+          ":linkedReports": [newLinkKey]
         }
       };
-      await dynamoDbLib.call("put", reportsParams);
-      await dynamoDbLib.call("update", updateStageParams);
-      return success(reportsParams.Item.reportId);
+      await dynamoDbLib.call("update", updateSellerReportParams);
     }
-    return failure({ status: false, error: "Item not found." });
+    return success(returnValues);
   } catch (e) {
+    console.log(e);
     return failure({ status: false });
   }
 }
