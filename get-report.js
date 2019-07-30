c4e748532ce117b89c116fcb613086015f6c63a7
Cole Marsteller <cgm32547@gmail.com>
2019-07-30 15:18:40 -0700

    make naming more specific

--- get-report.js
+++ get-report.js
@@ -2,21 +2,33 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
-  const params = {
+  const buyerReportParams = {
     TableName: "reports",
     Key: {
       reportId: event.pathParameters.id
     }
   };
+  const sellerReportParams = {
+    TableName: "stages",
+    Key: {
+      stageId: event.pathParameters.id
+    }
+  };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
+    const buyerReport = await dynamoDbLib.call("get", buyerReportParams);
+    if (buyerReport.Item) {
       // Return the retrieved item
-      return success(result.Item);
+      return success(buyerReport.Item);
+    }
+    const sellerReport = await dynamoDbLib.call("get", sellerReportParams);
+    if (sellerReport.Item) {
+      // Return the retrieved item
+      return success(sellerReport.Item);
     }
     return failure({ status: false, error: "Item not found." });
   } catch (e) {
+    console.log(e)
     return failure({ status: false });
   }
 }
