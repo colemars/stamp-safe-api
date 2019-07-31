5d5a3c10366e917183bd1f7baff377eb9ec01ecc
Cole Marsteller <cgm32547@gmail.com>
2019-07-31 13:31:04 -0700

    begin update getReport to also get reportStatus of child reports

--- get-report.js
+++ get-report.js
@@ -18,6 +18,25 @@ export default async function main(event) {
     }
   };
 
+  const deleteLinkedReport = async reportKey => {
+    const paramsQuery = {
+      TableName: process.env.tableName,
+      IndexName: "typeId-linkKey-index",
+      KeyConditionExpression: "typeId = :id and linkKey = :key",
+      ExpressionAttributeValues: {
+        ":id": buyerReport,
+        ":key": reportKey
+      }
+    };
+    try {
+      const results = await dynamoDbLib.call("query", paramsQuery);
+      const { reportStatus } = results.Items[0];
+      return reportStatus;
+    } catch (e) {
+      return failure({ status: false });
+    }
+  };
+
   try {
     const buyerResults = await dynamoDbLib.call("get", buyerReportParams);
     if (buyerResults.Item) {
@@ -25,6 +44,14 @@ export default async function main(event) {
     }
     const sellerResults = await dynamoDbLib.call("get", sellerReportParams);
     if (sellerResults.Item) {
+      const reportStatuses = [];
+      const results = await dynamoDbLib.call("get", buyerReportParams);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const linkedReport of results.Item.linkedReports) {
+        // eslint-disable-next-line no-await-in-loop
+        const reportStatus = await deleteLinkedReport(linkedReport);
+        reportStatuses.push(reportStatus);
+      }
       return success(sellerResults.Item);
     }
     return failure({ status: false, error: "Item not found." });
