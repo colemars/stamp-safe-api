5392a6c697171aad4c65f5928b04bed88abb5340
Cole Marsteller <cgm32547@gmail.com>
2019-07-30 22:35:01 -0700

    handle deleting all linked buyer reports if seller report is deleted

--- delete-report.js
+++ delete-report.js
@@ -1,16 +1,56 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+import { buyerReport, sellerReport } from "./constants/definitions";
 
 export default async function main(event) {
-  const params = {
-    TableName: "reports",
+  const data = JSON.parse(event.body);
+  const reportType = data.typeId === sellerReport ? sellerReport : buyerReport;
+  const paramsOriginal = {
+    TableName: "stampsafe-reports",
     Key: {
-      reportId: event.pathParameters.id
+      typeId: data.typeId,
+      accessKey: event.pathParameters.id
     }
   };
 
+  const deleteLinkedReport = async reportKey => {
+    const paramsQuery = {
+      TableName: "stampsafe-reports",
+      IndexName: "typeId-linkKey-index",
+      KeyConditionExpression: "typeId = :id and linkKey = :key",
+      ExpressionAttributeValues: {
+        ":id": buyerReport,
+        ":key": reportKey
+      }
+    };
+    try {
+      const results = await dynamoDbLib.call("query", paramsQuery);
+      const key = results.Items[0].accessKey;
+      const paramsDelete = {
+        TableName: "stampsafe-reports",
+        Key: {
+          typeId: buyerReport,
+          accessKey: key
+        }
+      };
+      await dynamoDbLib.call("delete", paramsDelete);
+      return success({ status: true });
+    } catch (e) {
+      return failure({ status: false });
+    }
+  };
+
+  if (reportType === sellerReport) {
+    const results = await dynamoDbLib.call("get", paramsOriginal);
+    for (const linkedReport of results.Item.linkedReports) {
+      await deleteLinkedReport(linkedReport);
+    }
+  }
+
   try {
-    await dynamoDbLib.call("delete", params);
+    await dynamoDbLib.call("delete", paramsOriginal);
     return success({ status: true });
   } catch (e) {
     return failure({ status: false });
