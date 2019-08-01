f2aa020ad9ba11fbf5b38e99899da4350a42df7d
Cole Marsteller <cgm32547@gmail.com>
2019-08-01 00:44:50 -0700

    remove error logging

--- helpers/hotgunz.js
+++ helpers/hotgunz.js
@@ -2,11 +2,8 @@ import fetch from "node-fetch";
 
 const getStolenRecord = serialNumber =>
   new Promise((resolve, reject) => {
-    console.log("start")
-    console.log(serialNumber)
     const params = new URLSearchParams();
-    console.log(params)
-    params.append("q", "MFG124");
+    params.append("q", serialNumber);
     const options = {
       method: "POST",
       body: params
@@ -16,6 +13,6 @@ const getStolenRecord = serialNumber =>
       // .then(checkStatus) It is common to create a helper function to check that the response contains no client (4xx) or server (5xx) error responses:
       .then(res => res.text())
       .then(body => resolve(body))
-      .catch(error => console.log(error));
+      .catch(error => reject(error));
   });
 export default getStolenRecord;
