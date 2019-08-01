d69f5e5c7ab3109781344ebf1b8558327c96592a
Cole Marsteller <cgm32547@gmail.com>
2019-07-31 22:30:33 -0700

    fix syntax

--- authorize-report.js
+++ authorize-report.js
@@ -24,7 +24,7 @@ export default async function main(event) {
           accessKey
         },
         UpdateExpression:
-          "SET accessToken = :accessToken, SET reportStatus = :reportStatus",
+          "SET accessToken = :accessToken, reportStatus = :reportStatus",
         ExpressionAttributeValues: {
           ":accessToken": "expired",
           ":reportStatus": "In Progress"
